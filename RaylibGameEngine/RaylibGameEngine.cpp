6921ec11cad6c5f0d7d926eeb254f6fb4ca600ea
JamesBakerAIE <s191482@students.aie.edu.au>
2022-08-17 15:35:09 +0930

    Added raylib & basic window.

---
+++ RaylibGameEngine/RaylibGameEngine.cpp
@@ -0,0 +1,46 @@
+// RaylibGameEngine.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "raylib.h"
+
+#include <iostream>
+
+int main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
